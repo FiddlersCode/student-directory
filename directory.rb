3b3d3a61797dc671389340c52a58bb1a5e616e9b
Paula <pmuldoon@umich.edu>
2017-03-28 11:12:12 +0100

    added each_with_index

--- directory.rb
+++ directory.rb
@@ -1,14 +1,16 @@
 def input_students
-  puts "Please enter the names of the students."
+  puts "Please enter the student's name."
   puts "To finish, just hit return twice."
   # create an empty array
   students = []
   # get the first namename = gets.chomp
   name = gets.chomp
+  puts "Please enter the student's cohort."
+  cohort = gets.chomp.to_sym
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student has to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # gets another name from the user
     name = gets.chomp
@@ -24,8 +26,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, i|
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
