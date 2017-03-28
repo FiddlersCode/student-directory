7093752c8ec20c78d11eb2e362ddcc37d3c3367f
Paula <pmuldoon@umich.edu>
2017-03-28 18:46:13 +0100

    Added loop to force user to input values for keys

--- directory.rb
+++ directory.rb
@@ -11,23 +11,30 @@ def input_students
   name = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
-    puts "Please enter the student's cohort."
+    puts "Please enter the student's cohort month."
     cohort = gets.chomp.capitalize
-    puts "Please enter the student's country of birth."
+      while cohort.empty? do
+        puts "You can't move on without adding the student's cohort month."
+        cohort = gets.chomp.capitalize
+      end
+    puts "Please enter the student's country or planet of birth."
     birth_country = gets.chomp.capitalize
+      while birth_country.empty? do
+        puts "You can't move on without adding the student's contry or planet of birth."
+        birth_country = gets.chomp.capitalize
+      end
     # add the student has to the array
     students << {name: name, cohort: cohort, birth_country: birth_country}
     if students.count == 1
-      puts "Now we have #{students.count} student.  Add another name or press return to finish."
+      puts "Now we have #{students.count} student.  \nAdd another name or press return to finish."
     else
-      puts "Now we have #{students.count} students. Add another name or press return to finish."
-  end
+      puts "Now we have #{students.count} students. \nAdd another name or press return to finish."
+    end
     # gets another name from the user
     name = gets.chomp.capitalize
   end
-  # return the aray of students
   students
-  end
+end
 
  # and then print them
 def print_header
@@ -39,12 +46,11 @@ def print(students)
   number_of_students = students.count
   n = 0
   while number_of_students > 0
-    puts "Student: #{students[n][:name]}\nCohort: #{students[n][:cohort]}\nCountry of Birth:#{students[n][:birth_country]}"
+    puts "Student: #{students[n][:name]}\nCohort: #{students[n][:cohort]}\nCountry of Birth: #{students[n][:birth_country]}"
     number_of_students -= 1
     n += 1
   end
 end
-
 
 def print_footer(names)
   if names.count == 0
