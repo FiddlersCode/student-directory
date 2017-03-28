5f89a36fbeffea073f05fb9b366c73be9b32f486
Paula <pmuldoon@umich.edu>
2017-03-28 12:06:11 +0100

    print specific letter start

--- directory.rb
+++ directory.rb
@@ -27,7 +27,9 @@ end
 
 def print(students)
   students.each_with_index do |student, i|
-    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name][0] == "A"
+      puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
